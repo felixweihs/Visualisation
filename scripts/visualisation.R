f41aaec2e41415e77e16ddd6cf41b1b1e2fb70d8
Weihs <wei115@csiro.au>
2019-10-03 16:39:32 +1000

    Added solutions to challenges 11 to 12 and script to adjust and save figures

--- scripts/visualisation.R
+++ scripts/visualisation.R
@@ -51,7 +51,7 @@ ggplot(
 ) + geom_line()
 
 #Challenge8
-gapminder_lifeExp <- gapminder %>% filter(contains(country == "A"))
+gapminder_lifeExp <- gapminder
 ggplot(
   data = gapminder_lifeExp, 
   mapping = aes(x = year, y = lifeExp, group = country, colour = continent)
@@ -72,25 +72,78 @@ ggplot(data = gapminder,
 
 #Challenge9
 ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp)) +
-  geom_point(colour = "black", size = 1.9) + 
+  geom_point(colour = "purple", size = 1.9) + 
   scale_x_log10() + 
   geom_smooth(method = "lm", size = 1.5)
 
 #Challenge10
-ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, colour = continent)) +
-  geom_point(shape = 18) + 
+ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, colour = continent, shape = continent)) +
+  geom_point() + 
   scale_x_log10() + 
   geom_smooth(method = "lm", size = 1.5)
 
 #Challenge11
-ggplot(data = gapminder, aes(x = gdpPercap, y = lifeExp, colour = continent)) +
+ggplot(
+  data = gapminder, 
+  mapping = aes(x = gdpPercap, y = lifeExp, colour = continent)) +
   geom_point(shape = 18, size = 2) + 
   scale_x_log10() + 
   geom_smooth(method = "lm", size = 1.5) +
   scale_colour_brewer(palette = "Dark2")
 
+#Challenge12 pre
+gapminder_A <- gapminder %>% filter(str_starts(country, "A"))
+ggplot(
+  data = gapminder_A, 
+  mapping = aes(x = year, y = lifeExp, group = country, colour = country)
+) + geom_line() + geom_point(colour = "black") + facet_wrap(~ country) + geom_smooth(method = "lm")
 
 
+gapminder_rich <-  filter(gapminder, gdpPercap > 40000)
+gapminder_poor <-  filter(gapminder, gdpPercap < 300)
+ggplot(
+  data = gapminder, 
+  mapping = aes(x = gdpPercap, y = lifeExp, colour = continent, size = pop, label = country)
+) + geom_point() + 
+  scale_x_log10() +
+  geom_text(data = gapminder_rich, size = 3, colour = "black") +
+  facet_wrap(~ year)
 
+#Challenge12
+ggplot(
+  data = gapminder, 
+  mapping = aes(x = pop,fill = continent)) +
+  geom_density(alpha = 0.5) +
+  scale_x_log10() +
+  facet_wrap(~year)
 
+#Output figure and clean-up labels and themes
+rough_plot <- ggplot(
+  data = gapminder, 
+  mapping = aes(x = gdpPercap, y = lifeExp, colour = continent, size = pop, label = country)
+) + geom_point() + 
+  scale_x_log10() +
+  geom_text(data = gapminder_rich, size = 3, colour = "black") +
+  geom_text(data = gapminder_poor, size = 3, colour = "black") +
+  facet_wrap(~ year)
 
+rough_plot + scale_colour_brewer(palette = "Set1")
+
+rough_plot +
+  labs(title = "Figure 1", 
+       x = "GDP/Capita", 
+       y = "Life Expectancy",
+       colour = "Continent",
+       caption = "Data source: Gapminder") +
+  theme(
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  plot.title = element_text(size = 20),
+  axis.title = element_text(size = 20),
+  axis.line = element_line(colour = "black", size = 1)
+)
+
+rough_plot + theme_linedraw()
+
+#Save plots
+ggsave("results/my_first_plot2.jpg", plot = rough_plot, width = 12, height = 12, units = "cm")
